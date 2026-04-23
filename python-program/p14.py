9b6d5babd7f652f179b88c6565a2968b1476dfbf
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-04-23 19:58:10 +0530

    Create p14.py
    Implement base conversion functions for binary, octal, and hexadecimal.

---
+++ python-program/p14.py
@@ -0,0 +1,17 @@
+#Base Conversion 
+# int,float,str, bool, NoneType....
+# number system changes functions : bin(), oct(), hex()
+
+no = int(input('Enter the No:'))
+print('no is :',no)
+print('type of no:',type(no))
+
+#base convert in some other base 
+print(f'Binary of {no} is {bin(no)}')
+print(f'Octal of {no} is {oct(no)}')
+print(f'Hexadecimal of {no} is {hex(no)}')
+
+
+
+
+
