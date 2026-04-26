c9042d5e3fa96c3dad00ab2e3f0f059d054d5cfe
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-04-26 13:03:15 +0530

    Create p18.py
    Add type hinting examples and comments in p18.py

---
+++ python-program/p18.py
@@ -0,0 +1,30 @@
+# Type Hinting 
+# Even thougt we donot declare type in python , # but after 3.5 version
+# python allows you declare type 
+# mypy : static type checker for python
+# for we use package called as pydantic : data validation and settings management using python type annotations
+
+
+#x= 'Awnish' # type of x is str
+# var_name : data_type = value
+
+
+x : str = 'Awnish'
+print(x)
+print(type(x))
+
+# # x=10
+# # print(x)
+# # print(type(x))
+
+# # x:int=10 # type -> int 
+# # print(x)
+# # print(type(x))
+
+# # from typing import Union
+
+# # x:Union[str,int] = 'Awnish'
+# # print(x)
+# print(type(x))
+
+
