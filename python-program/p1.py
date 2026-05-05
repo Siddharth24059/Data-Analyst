74ff267d87a22d1065fa483d5632cdc81e680d1c
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-05 18:33:43 +0530

    Update p1.py

--- python-program/p1.py
+++ python-program/p1.py
@@ -4,5 +4,5 @@ print(kw.kwlist)
 print('Total Keyword in python:',len(kw.kwlist))
 
 # There are 35 Keyword in python 
-# keywords : are the special words reserved in python for writing code 
+# keywords : are the special words reserved in python for writing code
 
