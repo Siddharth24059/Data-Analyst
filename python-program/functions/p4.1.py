14cc35cddd2c3b14ba4c9e5eaec9214ba3e8bd62
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-10 14:10:56 +0530

    Add files via upload

---
+++ python-program/functions/p4.1.py
@@ -0,0 +1,22 @@
+# Type of function on the basis of argument and return type 
+
+# ================= 4 Types are there =======================
+# No Argument and No Return Type 
+# with Argument and No Return type 
+# No Argument with Return Type
+# with Argument with Return Type 
+
+
+# No Argument and No Return Type
+
+import sys
+
+def wish():
+    print('hi good evening,welcome to sipher web academy')
+    
+    
+def main():
+    wish() 
+sys.exit(main())   
+
+
