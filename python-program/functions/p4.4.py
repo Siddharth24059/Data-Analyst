3fc202c7c328be74d677c802eef76ca4e36ec64e
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-11 14:16:52 +0530

    Add files via upload

---
+++ python-program/functions/p4.4.py
@@ -0,0 +1,14 @@
+# with argument with return type
+
+
+import sys
+
+def add(x,y):
+    return x+y
+
+    
+    
+def main():
+    result = add(10,20)
+    print(f'The result is: {result}')
+sys.exit(main())  
