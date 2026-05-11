02769b9fad6d530e0be34a22d07152282605ad72
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-11 14:15:50 +0530

    Add files via upload

---
+++ python-program/functions/p4.2.py
@@ -0,0 +1,13 @@
+#no argument with return type
+
+import sys
+
+def getGravity():
+     return 9.8
+ 
+ 
+def main():
+     print(f'the value of the gravity:{getGravity()}')
+    
+sys.exit(main())  
+
