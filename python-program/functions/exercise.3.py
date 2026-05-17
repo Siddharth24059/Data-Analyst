f8d70a3569986d98682ebbabc4b1319ec1c51dd1
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-18 03:51:56 +0530

    Add files via upload

---
+++ python-program/functions/exercise.3.py
@@ -0,0 +1,21 @@
+# make a cube list from odd numbers 1 to 100
+
+
+
+
+
+l=[]
+
+
+
+
+print('list:',1)
+
+for i in range(1,101):
+    l.append(i**3)
+    
+    
+print('list of cubes:',l)   
+
+
+ 
