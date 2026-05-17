57fdcc5cab1be07423e6f73ef781e289a4055f45
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-17 13:52:17 +0530

    Add files via upload

---
+++ python-program/functions/exercise.1.py
@@ -0,0 +1,20 @@
+#wap in python to generate the dictionary using 2 list
+
+
+
+l_keys = ['a','b','c','d','e','f','g']
+l_values=  [1,2,3,4,50,100,150]
+
+
+d = {}
+
+for i in range(len(l_keys)):
+    d[l_keys[i]]=l_values[i]
+
+print('d=',d)
+
+
+
+
+
+
