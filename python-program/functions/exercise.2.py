1b099fd29adb11b4d680e259e8cf650daf7bdc97
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-05-18 03:51:34 +0530

    Add files via upload

---
+++ python-program/functions/exercise.2.py
@@ -0,0 +1,19 @@
+# make a dynamic dictionary using list comprehension
+
+
+
+l=[]
+
+
+
+
+print('list:',1)
+
+for i in range(1,101):
+    l.append(i**2)
+    
+    
+print('list of squares:',l)   
+
+
+ 
