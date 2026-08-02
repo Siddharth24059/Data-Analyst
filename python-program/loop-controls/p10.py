46b59caa3bec356e585fdb306a0c3a63aad32eac
Siddharth jaiswal <siddharthjaiswal184@gmail.com>
2026-08-02 12:52:53 +0530

    Add files via upload

---
+++ python-program/loop-controls/p10.py
@@ -0,0 +1,11 @@
+# for loop in python 
+# use of for loop are normal loop then we use range 
+# we can maintain external counter with 
+# for loop but it is not recommended because for loop is used when we know the number of iterations
+
+
+counter = 1
+for i in range(1,10):
+    counter = counter+1
+    print(f'value of i = {i}')
+    print(f'counter = {counter}')
---
+++ python-program/loop-controls/p6.py
@@ -0,0 +1,25 @@
+# Types of loops 
+# Python two of loops 
+# for loop 
+# while
+#
+
+'''
+Syntax of while loop
+
+counter = initial value
+while condition:
+    # body of the loop
+    # update the counter
+
+''' 
+
+# we donot have do-while in python
+i=1 # start counter
+while(i<=5): # Falsey condition : when I is greater than 5 loop will stop
+    print(f'Hello = {i}')
+    i+=1 # update the counter : incrementing the counter by 1
+
+    # Infinite loop : loop which runs forever
+    # i = 1 will always be less than 5
+    # to avoid infinite loop we need to update the counter
